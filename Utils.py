801a39ffab9944b67947377d727d6d8073f61489
walterusrulz <zona_22@hotmail.com>
2020-10-20 11:14:43 +0200

    Patched Utils(pieces scale with boad size) and bug fix in Pawn class

--- Utils.py
+++ Utils.py
@@ -1,12 +1,11 @@
-#import java.text.DecimalFormat;
-#import java.util.ArrayList;
-#import java.util.Random;
+# import java.text.DecimalFormat;
+# import java.util.ArrayList;
+# import java.util.Random;
 
 import sys
 import random
 from Position import Position
 from State import State
-
 
 # all the pieces
 wPawn = 0
@@ -25,25 +24,27 @@ empty = 12
 
 # number of pieces
 diffPieces = 12
-numPieces = [8,2,2,2,1,1,8,2,2,2,1,1]
+numPieces = [8, 2, 2, 2, 1, 1, 8, 2, 2, 2, 1, 1]
 
 # name (and letter) of each piece
-names = ["wPawn", "wRook", "wBishop", "wkNnight", "wQueen", "wKing", "bPawn", "bRook", "bBishop", "bkNightight", "bQueen", "bKing"]
-letters = ["P","R","B","N","Q","K","p","r","b","n","q","k"," "]
+names = ["wPawn", "wRook", "wBishop", "wkNnight", "wQueen", "wKing", "bPawn", "bRook", "bBishop", "bkNightight",
+         "bQueen", "bKing"]
+letters = ["P", "R", "B", "N", "Q", "K", "p", "r", "b", "n", "q", "k", " "]
+
+
 # Note we use h for Horse instead of Knight
 # Note we add " " for empty cell
 
 # Get color piece
 def getColorPiece(piece):
-
-    if ((piece >=0) and (piece<=5)):
-    	return 0 #white
-    elif ((piece>5) and (piece<=11)):
-    	return 1 #black
+    if ((piece >= 0) and (piece <= 5)):
+        return 0  # white
+    elif ((piece > 5) and (piece <= 11)):
+        return 1  # black
     else:
-    	print("\n** Error, wrong piece code\n")
-    	sys.exit(0)
-    return -1  #never arrives here, just to avoid compilation error
+        print("\n** Error, wrong piece code\n")
+        sys.exit(0)
+    return -1  # never arrives here, just to avoid compilation error
 
 
 # This method generates a problem instance.
@@ -54,86 +55,89 @@ def getColorPiece(piece):
 # @return the initial state (board and agent)
 
 def getProblemInstance(n, p, seed, agent):
-   board = [[empty for i in range(n)] for j in range(n)]
+    board = [[empty for i in range(n)] for j in range(n)]
 
-   random.seed(seed)
+    random.seed(seed)
 
-   numPieces[agent] -= 1
-   allPositions = getAllBoardPositions(n)
+    numPieces[agent] -= 1
 
-   # placing our agent in the first row, we know these are the first n elements in allPositions
-   r = random.randint(0,n-1)
-   agentPos = allPositions.pop(r)
-   board[agentPos.row][agentPos.col] = agent
-		
-   # placing the rest of pieces
-   pos = None
-   for piece in range(diffPieces):
-      for j in range(numPieces[piece]):
-      	if (random.random()<=p):
-      		r = random.randint(0,len(allPositions)-1)
-      		pos = allPositions.pop(r)
-      		board[pos.row][pos.col] = piece
-		
-   # Creating the instance, i.e., the state
-   return State(board, agentPos, agent)
+    # adjusting the number of possible  pieces according to board's size
+    f = (n * n) / 64.0
 
+    for i in range(len(numPieces)):
+        numPieces[i] = round(numPieces[i] * f)
+
+    allPositions = getAllBoardPositions(n)
+
+    # placing our agent in the first row, we know these are the first n elements in allPositions
+    r = random.randint(0, n - 1)
+    agentPos = allPositions.pop(r)
+    board[agentPos.row][agentPos.col] = agent
+
+    # placing the rest of pieces
+    pos = None
+    for piece in range(diffPieces):
+        for j in range(numPieces[piece]):
+            if (random.random() <= p):
+                r = random.randint(0, len(allPositions) - 1)
+                pos = allPositions.pop(r)
+                board[pos.row][pos.col] = piece
+
+    # Creating the instance, i.e., the state
+    return State(board, agentPos, agent)
 
 
 #
 # fill (by rows) an ArrayList with all the possible coordinates
-# 
+#
 # @param n size of the board
 #
 
 def getAllBoardPositions(n):
-	return [Position(r,c) for r in range(n) for c in range(n)]
-	
+    return [Position(r, c) for r in range(n) for c in range(n)]
+
 
 #
 # Print a state (board + agent)
 #
 
 def printBoard(state):
-	#DecimalFormat df = new DecimalFormat("00");
-	size = len(state.m_board[0])
-	if (size>50):
-		print("**Error, board too large to be text-printed ...\n")
-		sys.exit(0)
+    # DecimalFormat df = new DecimalFormat("00");
+    size = state.m_boardSize
+    # if (size>50):
+    #	print("**Error, board too large to be text-printed ...\n")
+    #	sys.exit(0)
 
-	# upper row
-	print("   ", end="")
-	for c in range(size):
-		print("% 2d " % (c), end="")
-	print("")
+    # upper row
+    print("   ", end="")
+    for c in range(size):
+        print("% 2d " % (c), end="")
+    print("")
 
-	print("  ", end="")
-	for c in range(size):
-		print("---", end="")
-	print("--")
+    print("  ", end="")
+    for c in range(size):
+        print("---", end="")
+    print("--")
 
-	# board
-	for r in range(size):
-		print("% 2d|" % (r), end="")
-		for c in range(size): 
-			if (r == state.m_agentPos.row) and (c == state.m_agentPos.col):
-				print("*" + letters[state.m_board[r][c]]+"|", end="")
-			else:
-				print(" " + letters[state.m_board[r][c]]+"|", end="")
-		# bottom row
-		print("  ")
-		for c in range(size): 
-			print("---", end="")
-		print("--")
-
+    # board
+    for r in range(size):
+        print("% 2d|" % (r), end="")
+        for c in range(size):
+            if ((r == state.m_agentPos.row) and (c == state.m_agentPos.col)):
+                print("*" + letters[state.m_board[r][c]] + "|", end="")
+            else:
+                print(" " + letters[state.m_board[r][c]] + "|", end="")
+        # botton row
+        print("  ")
+        for c in range(size):
+            print("---", end="")
+        print("--")
 
 
 # main to test the methods
 
 if __name__ == '__main__':
+    st = getProblemInstance(8, 1.0, 1771, wRook)
+    print(st.m_board)
 
-	st = getProblemInstance(8, 1.0, 1771, wKing)
-	print(st.m_board)
-
-		
-	printBoard(st)
+    printBoard(st)
